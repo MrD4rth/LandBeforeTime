92537e12239d2649dc0a8abca1ad925db3760810
Brandon <d4rthtyran0us@gmail.com>
2014-12-15 00:05:30 -0500

    level files

---
+++ Dino_TestLevel/Dino_TestLevelMASTER/Assets/scripts/HealthBar.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour 
+{
+	public Texture2D frame;
+	public Rect framePosition;
+
+	public float horizontalDistance;
+	public float verticleDistance;
+	public float width;
+	public float height;
+	public Texture2D healthBar;
+	public Rect HealthBarPosition;
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+	
+	}
+
+	void OnGUI()
+	{
+		drawFrame ();
+		drawBar ();
+	}
+
+	void drawFrame()
+	{
+		framePosition.x = (Screen.width - framePosition.width) / 2;
+		framePosition.width = Screen.width * 0.38f;
+		framePosition.height = Screen.height * 0.048f;
+		GUI.DrawTexture (framePosition, frame);
+	}
+
+	void drawBar()
+	{
+		HealthBarPosition.x = framePosition.x + framePosition.width * horizontalDistance;
+		HealthBarPosition.y = framePosition.y + framePosition.height * verticleDistance;
+		HealthBarPosition.width = framePosition.width * width;
+		HealthBarPosition.height = framePosition.height * height;
+
+		GUI.DrawTexture (HealthBarPosition, healthBar);
+	}
+
+}
